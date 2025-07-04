1d33e0c3f7db63ceeb7f851d7c0588bfd9554fcc
Sans-arch <sansfleitas@gmail.com>
2025-07-04 20:23:31 -0300

    add collision mechanic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -17,7 +18,7 @@ def main():
     Asteroid.containers = (updatable_group, drawable_group, asteroids_group)
     AsteroidField.containers = (updatable_group) # type: ignore
     
-    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     AsteroidField()
     dt = 0
     
@@ -25,11 +26,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable_group.update(dt)
+        
+        for asteroid in asteroids_group:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
-
-        for updatable in updatable_group:
-            updatable.update(dt)
         
         for drawable in drawable_group:
             drawable.draw(screen)
