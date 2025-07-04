fec33581823265d96004fae6414b55ea8ad3f398
Sans-arch <sansfleitas@gmail.com>
2025-07-04 19:32:54 -0300

    create player movimentation when pressing w and s

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -8,7 +8,7 @@ class Player(CircleShape):
         self.rotation = 0
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)  # type: ignore
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -20,9 +20,19 @@ class Player(CircleShape):
 
     def __rotate__(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        
+    
+    def __move__(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_w]:
+            self.__move__(dt)
+            
+        if keys[pygame.K_s]:
+            self.__move__(-dt)
 
         if keys[pygame.K_a]:
             self.__rotate__(-dt)
