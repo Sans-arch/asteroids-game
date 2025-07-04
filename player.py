d868c0d9672892193f8fa8abed5a5b149b37c0d9
Sans-arch <sansfleitas@gmail.com>
2025-07-04 19:24:52 -0300

    create player rotation when pressing a and d

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -13,7 +13,19 @@ class Player(CircleShape):
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = self.position + forward * self.radius         # type: ignore
+        b = self.position - forward * self.radius - right # type: ignore
+        c = self.position - forward * self.radius + right # type: ignore
         return [a, b, c]
+
+    def __rotate__(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.__rotate__(-dt)
+
+        if keys[pygame.K_d]:
+            self.__rotate__(dt)
