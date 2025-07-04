4c5a8370e662b22255ecbd64db8fffaad1e17a30
Sans-arch <sansfleitas@gmail.com>
2025-07-04 19:13:50 -0300

    create circleshape and player classes

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
+        
+        # limit the framerate to 60FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
